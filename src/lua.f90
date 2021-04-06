5f1035b7570bc51a7f5c2e665f80ddc4b0cd808d
Bob Apthorpe <bob.apthorpe@gmail.com>
2021-04-06 12:45:57 -0500

    Added list of aux lib macros

--- src/lua.f90
+++ src/lua.f90
@@ -186,6 +186,24 @@ module lua
     ! luaL_unref
     ! luaL_where
 
+    ! Macros from lauxlib.h:
+    ! #define luaL_newlibtable(L,l)    \
+    !   lua_createtable(L, 0, sizeof(l)/sizeof((l)[0]) - 1)
+    ! #define luaL_newlib(L,l)  \
+    !   (luaL_checkversion(L), luaL_newlibtable(L,l), luaL_setfuncs(L,l,0))
+    ! #define luaL_argcheck(L, cond,arg,extramsg)    \
+    !         ((void)((cond) || luaL_argerror(L, (arg), (extramsg))))
+    ! #define luaL_checkstring(L,n)    (luaL_checklstring(L, (n), NULL))
+    ! #define luaL_optstring(L,n,d)    (luaL_optlstring(L, (n), (d), NULL))
+    ! #define luaL_typename(L,i)    lua_typename(L, lua_type(L,(i)))
+    ! #define luaL_dofile(L, fn) \
+    !     (luaL_loadfile(L, fn) || lua_pcall(L, 0, LUA_MULTRET, 0))
+    ! #define luaL_dostring(L, s) \
+    !     (luaL_loadstring(L, s) || lua_pcall(L, 0, LUA_MULTRET, 0))
+    ! #define luaL_getmetatable(L,n)    (lua_getfield(L, LUA_REGISTRYINDEX, (n)))
+    ! #define luaL_opt(L,f,n,d)    (lua_isnoneornil(L,(n)) ? (d) : f(L,(n)))
+    ! #define luaL_loadbuffer(L,s,sz,n)    luaL_loadbufferx(L,s,sz,n,NULL)
+
     ! Implemented C API Functions
     public :: lua_absindex
     public :: lua_arith
@@ -2223,6 +2241,7 @@ module lua
 
         !***** Not used; the C version of `luaL_argcheck` is implemented
         !***** as a macro so this interface cannot be bound to the C API
+
         ! !> @brief Checks whether `cond` is true. If it is not, raises an
         ! !! error reporting a problem with argument `arg` of the
         ! !! C function that called it, using a standard message that
