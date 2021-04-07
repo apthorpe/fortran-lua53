2d56ed073f1527ba283d3ecaebfca8c095729864
Bob Apthorpe <bob.apthorpe@gmail.com>
2021-04-06 21:19:04 -0500

    Added dot notation tests

--- test/tablecheck/tablecheck.lua
+++ test/tablecheck/tablecheck.lua
@@ -24,7 +24,8 @@ gv2d = { 5; 4; 3, 2, 1, }
 gv3a = { ["a"] = 5, ["b"] = 4, ["c"] = 3, ["d"] = 2, ["e"] = 1 }
 
 -- gv3b is a dict identical to gv3a
-gv3b = { a = 5, b = 4, c = 3, d = 2, e = 1 }
+gv3b = { a = 5, b = 4, c = 0, d = 2, e = 1 }
+gv3b.c = 3
 
 -- -- gv3c is a dict identical to gv3a
 -- gv3c = { ["a"] = 5, b = 4; "c" = 3, d = 2; ["e"] = 1; }
@@ -33,6 +34,7 @@ gv3b = { a = 5, b = 4, c = 3, d = 2, e = 1 }
 gv4a = { "Hydraulic pump", 124.95, -2, "Backordered", { bearings = 2, seals = 8, impeller = 1 }, false }
 
 -- gv4b is a mixed array
-gv4b = { "Hydraulic pump", 124.95, -2, status = "Backordered", partslist = { bearings = 2, seals = 8, impeller = 1 }, false }
+gv4b = { "Hydraulic pump", 124.95, -2, status = "Backordered", partslist = {bearings = 20, seals = 8, impeller = 1 }, false }
+gv4b.partslist.bearings = 2
 
 
